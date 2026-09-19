21f2941b9e82ff566da1aefdc8b6b911ad5ddc79
Ikfiny <149261344+Ikfini-101@users.noreply.github.com>
2026-09-19 23:17:44 +0000

    UI: Remove 'Profil & Parcours' link from navbar and footer

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,6 +1,5 @@
 ﻿const navBarLinks = [
   { name: "Accueil", url: "/" },
-  { name: "Profil & Parcours", url: "/#profil" },
   { name: "Publications", url: "/publications" },
 ];
 
@@ -9,7 +8,6 @@ const footerLinks = [
     section: "Parcours",
     links: [
       { name: "Accueil", url: "/" },
-      { name: "Profil & Parcours", url: "/#profil" },
       { name: "Publications Scientifiques", url: "/publications" },
     ],
   }
